bb50991893480472048e281639438938d053435a
JohanJianta <johanreinaldojianta@gmail.com>
2023-12-12 12:00:46 +0800

    ubah sedikit

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "Rest-Api-DBJewelry/apiJson"
+import "Rest-Api-DBJewelry/apiMySql"
 
 func main() {
 	// Rest API dengan database file json
-	apiJson.Init()
+	// apiJson.Init()
 
 	// Rest API dengan database mysql di localhost
-	// apiMySql.Init()
+	apiMySql.Init()
 }
